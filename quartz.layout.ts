bf19389acb5479359b52a11c6f0aecef7977763a
STRRL <im@strrl.dev>
2025-01-04 15:10:11 -0800

    feat: setup giscus
    Signed-off-by: STRRL <im@strrl.dev>

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'STRRL/quartz',
+        // from data-repo-id
+        repoId: 'R_kgDONl8eCQ',
+        // from data-category
+        category: 'General',
+        // from data-category-id
+        categoryId: 'DIC_kwDONl8eCc4ClucV',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
