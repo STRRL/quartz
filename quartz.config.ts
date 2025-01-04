8d3fb4486e7b829c915c29fed2d26387ce9f2af7
STRRL <im@strrl.dev>
2025-01-04 14:59:55 -0800

    feat: update configuration
    Signed-off-by: STRRL <im@strrl.dev>

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,16 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "What STRRL Known",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-08CC9PML1V",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "whatiknown.strrl.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
