4c48fd6d2721942c99eaa71e45b0f5c1163b4b81
Zhiqiang ZHOU <im@strrl.dev>
2026-02-28 19:06:00 -0800

    remove Recent Thoughts section from layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,11 +6,6 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [
-    Component.RecentNotes({
-      title: "Recent Thoughts",
-      limit: 20,
-      showTags: true,
-    }),
     Component.Comments({
       provider: "giscus",
       options: {
